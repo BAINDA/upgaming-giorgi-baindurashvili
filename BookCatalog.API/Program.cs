c8d90bc7846d7853c306bd41c3351c46c5b45140
Bainda <giorgibainda14@gmail.com>
2025-10-27 14:27:25 +0400

    integrate swashbuckle to enable API test and also configured dependency injection for services

--- BookCatalog.API/Program.cs
+++ BookCatalog.API/Program.cs
@@ -1,10 +1,29 @@
-
+using BookCatalog.Application.Repositories;
+using BookCatalog.Application.Services.Implementations;
+using BookCatalog.Application.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+// swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DI registrations
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IBookService, BookService>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// enable swagger UI in Development (or always if you want)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
